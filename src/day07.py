644dad1c61695502bc7f90bece8913b6071fcbdb
Surya Dutta <surya.dutta@spothero.com>
2022-12-07 12:10:35 -0600

    more cleanup

--- src/day07.py
+++ src/day07.py
@@ -59,12 +59,6 @@ class Directory:
                 yield subsubdirectory
 
 
-def nested_set(dic, keys, value):
-    for key in keys[:-1]:
-        dic = dic.setdefault(key, {})
-    dic[keys[-1]] = value
-
-
 class FileManager:
 
     root_directory: Directory
